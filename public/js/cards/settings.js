920bf100615aaf14f925545593dc560f71843438
robin <al4crity@gmail.com>
2016-05-27 19:33:49 +0200

    Added call to shutdown on shutdown button press.

--- public/js/cards/settings.js
+++ public/js/cards/settings.js
@@ -2,6 +2,14 @@ var settings = (function(){
 
   function init(){
     // Bind settings and stuff.
+
+    var powerButton = document.getElementById("powerSettingsButton");
+
+    powerButton.onclick = function(){
+    	$.post('/shutdown').done(function(){
+    		// Well, what can you do. We're shutting down.
+    	});
+    }
   }
 
   return {
